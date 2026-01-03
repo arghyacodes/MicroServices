5fb712866f21bdbf6459082d73a9300eb7606527
arghyacodes <arghyaghoshal.juetce@gmail.com>
2026-01-03 17:10:11 +0530

    Problem 2

---
+++ ebookstoreapp/src/main/java/com/example/ebookstoreapp/EbookstoreappApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebookstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookstoreappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookstoreappApplication.class, args);
+	}
+
+}
